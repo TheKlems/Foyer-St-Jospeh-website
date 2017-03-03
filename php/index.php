2f9578ba71a6ba09dddd6813ddba2d18ec01978d
Brodcouille <vincent.brodard@gmail.com>
2017-03-03 15:29:31 +0100

    glyphicons work now

--- php/index.php
+++ php/index.php
@@ -13,25 +13,46 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">Project name</a>
+              <a class="navbar-brand" href="#">Foyer St-Joseph</a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+
                 <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">A propos du foyer<span class="caret"></span></a>
                   <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li class="dropdown-header">Nav header</li>
-                    <li><a href="#">Separated link</a></li>
-                    <li><a href="#">One more separated link</a></li>
+                    <li><a href="#">Historique</a></li>
+                    <li><a href="#">Philosophie</a></li>
+                    <li><a href="#">Organisation</a></li>
                   </ul>
                 </li>
+                
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Prestation<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Les séjours</a></li>
+                    <li><a href="#">Soins d'accompagnement</a></li>
+                    <li><a href="#">Animation</a></li>
+                    <li><a href="#">Les repas</a></li>
+                    <li><a href="#">Entretien du linge</a></li>
+                    <li><a href="#">Accueil extra scolaire</a></li>
+                  </ul>
+                </li>
+
+                <li><a href="#contact">Demande d'admission</a></li>
+
+                <li><a href="#contact">Liens</a></li>
+                <li><a href="#contact">Photos</a></li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contact<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Où nous trouver ?</a></li>
+                    <li><a href="#">Proposition/Réclamation</a></li>
+                  </ul>
+                </li>
+
               </ul>
             </div>
           </div>
